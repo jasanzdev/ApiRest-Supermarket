03c985ef60860c2d6ba00b9033b24b4649eaa222
Jose Alejandro Sanz Cuesta <sanz_cuesta94@hotmail.com>
2025-02-25 16:44:21 -0500

    Delete products/dist directory

--- products/dist/constants/roles.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Roles = void 0;
-exports.Roles = ['ADMIN', 'SUPERVISOR', 'OPERATOR'];
--- products/dist/controllers/RefreshToken.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RefreshToken = void 0;
-const RefreshToken = (req, res) => {
-};
-exports.RefreshToken = RefreshToken;
